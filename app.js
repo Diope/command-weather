84a9cc8ea3e5664e774eea7e8a125dd4892fec6c
Dion Pettaway <dionp@fullsail.edu>
2017-05-24 01:36:03 -0400

    Added command line alias

--- app.js
+++ app.js
@@ -1,7 +1,24 @@
 const request = require('request');
+const yargs = require('yargs');
+
+const argv = yargs
+    .options({
+        a: {
+            demand: true,
+            alias: 'address',
+            describe: 'Address to fetch weather for',
+            string: true
+        }
+    })
+    .help()
+    .alias()
+    .argv;
 
 request({
-    url: 'https://maps.googleapis.com/maps/api/geocode/json?address='
+    url: 'https://maps.googleapis.com/maps/api/geocode/json?address=1301%20lombard%20street%20PA',
+    json: true
 }, (err, res, body) => {
-
+    console.log(`Address: ${body.results[0].formatted_address}`);
+    console.log(`Latitude: ${body.results[0].geometry.location.lat}`);
+    console.log(`Longitude: ${body.results[0].geometry.location.lng}`);
 })
---
+++ readme.md
@@ -0,0 +1,5 @@
+#Command Weather
+
+A small CLI for grabbing the current weather from whatever location you want. Simply type `node app.js a` + the location of your choosing.
+
+That's all it does, dats it!
