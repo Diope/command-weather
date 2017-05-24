99e74bb024c3504a7a06ff59b1a0e06e3c6aba2b
Dion Pettaway <dionp@fullsail.edu>
2017-05-24 02:20:36 -0400

    EncodeURIComponent added so users can enter in their own address

--- app.js
+++ app.js
@@ -11,11 +11,13 @@ const argv = yargs
         }
     })
     .help()
-    .alias()
+    .alias('help', 'h')
     .argv;
 
+    var encodedAddress = encodeURIComponent(argv.address);
+
 request({
-    url: 'https://maps.googleapis.com/maps/api/geocode/json?address=1301%20lombard%20street%20PA',
+    url: `https://maps.googleapis.com/maps/api/geocode/json?address=${encodedAddress}`,
     json: true
 }, (err, res, body) => {
     console.log(`Address: ${body.results[0].formatted_address}`);
